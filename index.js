7c1577c3d0f9c2c3cb553c2cab062a3bbea7c4f8
Prachi <prachi3761@gmail.com>
2026-04-09 20:08:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -549,5 +549,23 @@ console.log(d.getDate());
 /// intro to event 
 
 // event means ki agar button par click  karo to vo react karen
-    
+    // basic calculator 
+const button  = document.querySelector('button') ;
+
+button.addEventListener('click',()=>{
+
+  const  input1 = document.getElementById('first');
+  const number1 = Number(input1.value);
+
+  const  input2 = document.getElementById('second');
+  const number2 = Number(input2.value);
+
+
+  const result= number1+number2;
+   const re = document.getElementById('result');
+   re.textContent= "Result:"+result;
+})
+
+
+
 
