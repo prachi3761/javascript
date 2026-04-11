e3a56c2d5d6ba170281b92a1d20f46144d310cc9
Prachi <prachi3761@gmail.com>
2026-04-11 21:32:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -568,4 +568,28 @@ button.addEventListener('click',()=>{
 
 
 
+   /// making multiple circles in background 
+document .body.addEventListener("click", (event)=>  {
+   const circle = document.createElement('div');
+
+      circle.className = 'circle';
+
+      circle.textContent="hi";
+
+
+      const x =event.clientX;
+      const y = event.clientY;
+
+      circle.style.left = '${x-25}px'
+
+      circle.style.top = '${y-25}px'
+   const color = ["red","blue", "orange", "green", "purple", "white"];
+
+   circle.style.backgroundColor = color[Math.floor(Math.random()*color.length)];
+      document.body.appendChild(circle);
+});
+
+    
+
+
 
