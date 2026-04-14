90f2a6b1d9a5e9d323512433dc417f3aa88613e9
Prachi <prachi3761@gmail.com>
2026-04-14 12:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -589,6 +589,41 @@ document .body.addEventListener("click", (event)=>  {
       document.body.appendChild(circle);
 });
 
+
+    //button ka backgorund color change kar sakte agar botton par click kiya same color of button  h ussi ka same background ka ho jayega 
+      const red =  document.getElementById('red');
+
+  const blue =  document.getElementById('blue');
+   const orange =  document.getElementById('orange');
+    const green =  document.getElementById('green');
+     const purple =  document.getElementById('purple');
+
+ const body = document.body;
+
+    
+  red.addEventListener('click',()=> {
+      body.style.backgroundColor = 'red';
+
+     })
+
+      blue.addEventListener('click',()=> {
+      body.style.backgroundColor = 'blue';
+
+     })
+      orange.addEventListener('click',()=> {
+      body.style.backgroundColor = 'orange';
+
+     })
+      green.addEventListener('click',()=> {
+      body.style.backgroundColor = 'green';
+
+     })
+
+      purple.addEventListener('click',()=> {
+      body.style.backgroundColor = 'purple';
+
+     })
+    
     
 
 
